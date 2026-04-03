e0bbd5faea9527881d45922378d4bfc937ed08df
G7OSTIQ <106563737+G7OSTIQ@users.noreply.github.com>
2026-04-03 14:36:03 +0200

    platform updated

--- Assets/scripts/platform_script.cs
+++ Assets/scripts/platform_script.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class platform_script : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float jumpForce = 10f;
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.relativeVelocity.y >= 0f)
+        {
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                Vector2 velocity = rb.linearVelocity;
+                velocity.y = jumpForce;
+                rb.linearVelocity = velocity;
+            }
+        }
     }
 }
