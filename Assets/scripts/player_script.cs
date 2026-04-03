4a3ee89add6f89ad5fdf558d3862eaf1aa1d9aac
G7OSTIQ <106563737+G7OSTIQ@users.noreply.github.com>
2026-04-03 14:28:53 +0200

    player movement added

--- Assets/scripts/player_script.cs
+++ Assets/scripts/player_script.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class player_script : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float speed = 10f;
+    public Rigidbody2D rb;
+    private float moveX;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        moveX = Keyboard.current.dKey.isPressed ? 1f :
+            Keyboard.current.aKey.isPressed ? -1f : 0f;
+        moveX *= speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveX, rb.linearVelocity.y);
     }
 }
